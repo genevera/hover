de4587f1158644e905b350ef028a3ca22c771667
genevera <jltgnyc+github@gmail.com>
2016-05-26 16:27:40 -0400

    make this work again. ps shut up, rubocop

--- lib/hover/api.rb
+++ lib/hover/api.rb
@@ -1,10 +1,11 @@
+# API class
 require 'rest_client'
 require 'json'
 
 module Hover
   class Api
-    URL = 'https://www.hover.com/api/'
-    AUTH_URL = 'https://www.hover.com/signin'
+    URL = 'https://www.hover.com/api/'.freeze
+    AUTH_URL = 'https://www.hover.com/signin/auth.json'.freeze
 
     attr_accessor :client, :username, :password
 
@@ -12,54 +13,57 @@ module Hover
       self.username = username
       self.password = password
 
-      session = authenticate(username, password)
-      self.client = RestClient::Resource.new(URL, :cookies => {:hoverauth => session})
+      begin
+        session = authenticate(username, password)
+      rescue StandardError => e
+        puts e.backtrace
+      end
+
+      self.client = RestClient::Resource.new(URL,
+                                             cookies: { hoverauth: session })
     end
 
     def authenticate(username, password)
-      auth = {username: username, password: password}
-      RestClient.post(AUTH_URL, auth) do |response, request, result|
-        if response.code == 302
-          return response.cookies["hoverauth"]
-        else
-          raise "Failed to authenticate"
-        end
+      auth = { username: username, password: password }
+      RestClient.post(AUTH_URL, auth) do |response, _request, _result|
+        raise StandardError, 'Failed to authenticate' unless response.code == 200
+        response.cookies['hoverauth']
       end
     end
 
     def domains(params = {})
-      response = get("domains", params)
-      JSON.parse(response)["domains"]
+      response = get('domains', params)
+      JSON.parse(response)['domains']
     end
 
     def dns(params = {})
-      response = get("dns", params)
-      JSON.parse(response)["domains"]
+      response = get('dns', params)
+      JSON.parse(response)['domains']
     end
 
-    def domain(id, params={})
+    def domain(id, params = {})
       response = get("domains/#{id}", params)
-      JSON.parse(response)["domain"]
+      JSON.parse(response)['domain']
     end
 
-    def domain_dns(id, params={})
+    def domain_dns(id, params = {})
       response = get("domains/#{id}/dns", params)
-      JSON.parse(response)["domains"].first["entries"]
+      JSON.parse(response)['domains'].first['entries']
     end
 
     def create_record(id, name, type, content)
-      params = {:name => name, :type => type, :content => content}
+      params = { name: name, type: type, content: content }
       response = post("domains/#{id}/dns", params)
       JSON.parse(response)
     end
 
-    def update_record(id, params={})
+    def update_record(id, params = {})
       response = put("dns/#{id}", params)
       JSON.parse(response)
     end
 
     [:get, :put, :post, :delete].each do |method|
-      define_method(method) do |path, params={}|
+      define_method(method) do |path, params = {}|
         client[path].send(method, params)
       end
     end
